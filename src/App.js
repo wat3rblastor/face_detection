700909c073a5fcc54e079510ab99f16e4f4b53b2
Bowen Cheng <bowencheng2005@gmail.com>
2024-05-11 17:04:54 -0500

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
+// import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
 // import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
